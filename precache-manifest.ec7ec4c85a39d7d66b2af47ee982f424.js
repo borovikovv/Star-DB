8b7fd141258c8a1cccb2d6c5b1f0ed09a18e5690
Aleksey <43148382+borovikovv@users.noreply.github.com>
2019-07-16 15:19:30 +0300

    Updates

---
+++ precache-manifest.ec7ec4c85a39d7d66b2af47ee982f424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "033b93dc27e70fb99be4a240c35f7c0b",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "c2764e73cbecad38a6be",
+    "url": "/star-db/static/css/main.48f30647.chunk.css"
+  },
+  {
+    "revision": "1409c9d285d28d0fc00d",
+    "url": "/star-db/static/js/2.fe36e554.chunk.js"
+  },
+  {
+    "revision": "c2764e73cbecad38a6be",
+    "url": "/star-db/static/js/main.4cf99fc2.chunk.js"
+  },
+  {
+    "revision": "7fd0c1ce602d36977d9a",
+    "url": "/star-db/static/js/runtime~main.82ef0156.js"
+  },
+  {
+    "revision": "e76c8d59f36b95f77fda8e1c7e231688",
+    "url": "/star-db/static/media/Death-Star-icon.e76c8d59.png"
+  }
+]);
